9fa328fcc326a52bd394c2804ce8d03c618809d8
TugceGultekn <97016543+TugceGultekn@users.noreply.github.com>
2022-04-12 14:17:45 +0300

    303 logosu eklendi. ProductController eklendi. CRUD işlemlerine başlayacağız.

---
+++ ECommerceLiteUI/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceLiteUI.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        //bu controllera admin gibi yetkili kişiler erişecek. burada ürünlerin listelenmesi. ekleme silme güncelleme işlemleri
+        //yapılacaktır.
+        public ActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
