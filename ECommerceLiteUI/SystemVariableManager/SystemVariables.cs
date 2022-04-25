22e75a5f19b402c711b216b04a58675fa4108e79
TugceGultekn <97016543+TugceGultekn@users.noreply.github.com>
2022-04-25 11:46:13 +0300

    Utilities eklendi. WebConfig içindeki AppSettings içine eklenilen bilgilere SystemConfiguration ile nasıl erişebileceğimiz işlendi.

---
+++ ECommerceLiteUI/SystemVariableManager/SystemVariables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace ECommerceLiteUI.SystemVariableManager
+{
+    public static class SystemVariables
+    {
+        public static string EMAİL
+        {
+            get
+            {
+                try
+                {
+                    return
+                       ConfigurationManager.AppSettings["ECommerceLiteMail"].ToString();
+                }
+                catch (Exception)
+                {
+
+                    throw new Exception("HATA:WebConfig dosyasında email bilgisi bulunamadı.");
+                }
+            }
+        }
+    }
+}
